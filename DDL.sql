7944a5306407b75bdee02a15a059d8db5c90031e
mrivanlima <mrivanlima@gmail.com>
2024-05-20 20:01:16 -0400

    Add DDL Functions

--- DDL.sql
+++ DDL.sql
@@ -1,24 +1,5 @@
 create schema if not exists app;
 
-CREATE OR REPLACE FUNCTION drop_all_foreign_keys() RETURNS VOID AS $$
-DECLARE
-    fk_record RECORD;
-    drop_stmt TEXT;
-BEGIN
-    FOR fk_record IN (
-        SELECT conrelid::regclass AS table_from,
-               conname AS constraint_def
-        FROM pg_constraint
-        WHERE contype = 'f'
-    )
-    LOOP
-        drop_stmt := 'ALTER TABLE ' || fk_record.table_from || ' DROP CONSTRAINT ' || quote_ident(fk_record.constraint_def);
-        raise notice 'Droping FK %', drop_stmt;
-        EXECUTE drop_stmt;
-    END LOOP;
-END;
-$$ LANGUAGE plpgsql;
-
 SELECT drop_all_foreign_keys();
 
 drop table if exists app.state;
