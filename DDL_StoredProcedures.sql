5e5fe9572a85819eed4abe155010981057950719
mrivanlima <mrivanlima@gmail.com>
2024-05-27 12:32:20 -0400

    Seed sp

--- DDL_StoredProcedures.sql
+++ DDL_StoredProcedures.sql
@@ -914,6 +914,18 @@ $$ LANGUAGE plpgsql;
 
 
 
+CREATE OR REPLACE PROCEDURE app.usp_seed()
+AS $$	
+BEGIN
+	CALL app.usp_state_import();
+	CALL app.usp_city_import();
+	CALL app.usp_neighborhood_import();
+	CALL app.usp_street_import();
+END;
+$$ LANGUAGE plpgsql;
+
+
+
 
 
 
---
+++ seed.sql
@@ -0,0 +1,1 @@
+CALL app.usp_seed()
