ee22d9386517055abda1ec160bd9bb42c3130609
mrivanlima <mrivanlima@gmail.com>
2024-06-10 16:45:02 -0400

    Organizing files

--- master_script.sql
+++ master_script.sql
@@ -1,8 +1,12 @@
 \i 'C:/Development/EconomizzeDB/schemas.sql'
 \i 'C:/Development/EconomizzeDB/extensions.sql'
 \i 'C:/Development/EconomizzeDB/DropTable.sql'
+
+--Add imp tables
 \i 'C:/Development/EconomizzeDB/imp/zip_info.sql'
 
+
+--Add app tables
 \i 'C:/Development/EconomizzeDB/app/Tables/error_log.sql'
 \i 'C:/Development/EconomizzeDB/app/Tables/state.sql'
 \i 'C:/Development/EconomizzeDB/app/Tables/city.sql'
@@ -29,13 +33,38 @@
 \i 'C:/Development/EconomizzeDB/app/Tables/contact_type.sql'
 
 
+--Add app functions
 
+\i 'C:\Development/EconomizzeDB/app/Functions/drop_all_foreign_keys.sql'
+\i 'C:\Development/EconomizzeDB/app/Functions/usp_api_state_read_all.sql'
+\i 'C:\Development/EconomizzeDB/app/Functions/usp_api_state_read_by_id.sql'
 
+--Add Stored Procedures
 
+\i 'C:/Development/EconomizzeDB/app/StoredProcedures/drop_foreign_keys.sql'
+\i 'C:/Development/EconomizzeDB/app/StoredProcedures/usp_api_address_create.sql'
+\i 'C:/Development/EconomizzeDB/app/StoredProcedures/usp_api_address_type_create.sql'
+\i 'C:/Development/EconomizzeDB/app/StoredProcedures/usp_api_city_create.sql'
+\i 'C:/Development/EconomizzeDB/app/StoredProcedures/usp_api_drugstore_create.sql'
+\i 'C:/Development/EconomizzeDB/app/StoredProcedures/usp_api_neighborhood_create.sql'
+\i 'C:/Development/EconomizzeDB/app/StoredProcedures/usp_api_state_create.sql'
+\i 'C:/Development/EconomizzeDB/app/StoredProcedures/usp_api_state_update_by_id.sql'
+\i 'C:/Development/EconomizzeDB/app/StoredProcedures/usp_api_street_create.sql'
+\i 'C:/Development/EconomizzeDB/app/StoredProcedures/usp_api_user_address_create.sql'
+\i 'C:/Development/EconomizzeDB/app/StoredProcedures/usp_api_user_create.sql'
+\i 'C:/Development/EconomizzeDB/app/StoredProcedures/usp_api_user_login_create.sql'
+\i 'C:/Development/EconomizzeDB/app/StoredProcedures/usp_api_user_setup.sql'
+\i 'C:/Development/EconomizzeDB/app/StoredProcedures/usp_city_import.sql'
+\i 'C:/Development/EconomizzeDB/app/StoredProcedures/usp_neighborhood_import.sql'
+\i 'C:/Development/EconomizzeDB/app/StoredProcedures/usp_seed.sql'
+\i 'C:/Development/EconomizzeDB/app/StoredProcedures/usp_state_import.sql'
+\i 'C:/Development/EconomizzeDB/app/StoredProcedures/usp_street_import.sql'
 
+--Add Views
 
+\i 'C:/Development/EconomizzeDB/app/Views/v_street_details.sql'
+\i 'C:/Development/EconomizzeDB/app/Views/v_user_login_details.sql'
 
-
-
-\i 'C:/Development/EconomizzeDB/app_DDL.sql'
-\i 'C:/Development/EconomizzeDB/DDL_Functions.sql'
+--Add Indexes
+\i 'C:/Development/EconomizzeDB/app/Indexes/idx_street_zipcode.sql'
+\i 'C:/Development/EconomizzeDB/app/Indexes/ix_userlogin_username.sql'
