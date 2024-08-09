2bacb0bc869ad98103025b85d4dd2a685d8f24a2
Ivan Lima <mrivanlima@gmail.com>
2024-08-09 18:34:28 -0400

    Change phone number restrictions

--- app/StoredProcedures/usp_api_user_create.sql
+++ app/StoredProcedures/usp_api_user_create.sql
@@ -3,13 +3,12 @@
 -----------------------------------------------------------------
 DROP PROCEDURE IF EXISTS app.usp_api_user_create;
 CREATE OR REPLACE PROCEDURE app.usp_api_user_create(
-    OUT p_out_user_id INTEGER,
+    IN p_user_id INTEGER,
 	OUT p_out_message VARCHAR(100),
     IN p_user_first_name VARCHAR(100),
-    IN p_user_email VARCHAR(250),
-    IN p_user_unique_id UUID,
     IN p_user_middle_name VARCHAR(100) DEFAULT NULL,
     IN p_user_last_name VARCHAR(100) DEFAULT NULL,
+    IN p_phone_number VARCHAR(15) DEFAULT NULL,
     IN p_cpf CHAR(11) DEFAULT NULL,
     IN p_rg VARCHAR(100) DEFAULT NULL,
     IN p_date_of_birth DATE DEFAULT NULL,
@@ -26,47 +25,33 @@ BEGIN
     p_user_middle_name := TRIM(p_user_middle_name);
     p_user_last_name := TRIM(p_user_last_name);
     p_user_email := TRIM(p_user_email);
+    p_phone_number := TRIM(p_phone_number);
     p_cpf := TRIM(p_cpf);
     p_rg := TRIM(p_rg);
    
     BEGIN
-        -- Check for existing email
-        SELECT user_id INTO p_out_user_id
-        FROM app.user
-        WHERE user_email = p_user_email; 
-
-        IF p_out_user_id IS NOT NULL THEN
-            p_out_message :=  'Email ja cadastrado!';
-			RAISE EXCEPTION USING MESSAGE = p_out_message;
-        END IF;
-
-        -- Check CPF length
-        -- IF p_cpf IS NOT NULL AND l_is_match THEN
-        --     p_out_message := 'CPF invalido!';
-        --     RAISE EXCEPTION USING MESSAGE = p_out_message;
-        -- END IF;
 
         -- Insert new user
         INSERT INTO app.user
         (
+            user_id,
             user_first_name,
             user_middle_name,
             user_last_name,
-            user_email,
             cpf,
             rg,
-            user_unique_id,
+            phone_number,
             date_of_birth
         ) 
         VALUES 
         (
+            p_user_id,
             p_user_first_name,
             p_user_middle_name,
             p_user_last_name,
-            p_user_email,
             p_cpf,
             p_rg,
-            p_user_unique_id,
+            p_phone_number,
             p_date_of_birth
         );  
 
--- app/Tables/user.sql
+++ app/Tables/user.sql
@@ -8,7 +8,7 @@ create table app.user
 	cpf char(11) null,
 	rg  varchar(100) null,
 	date_of_birth date null,  
-	phone_number VARCHAR(11) null, 
+	phone_number VARCHAR(15) null, 
 	created_by integer null,
 	created_on 	timestamp with time zone default current_timestamp,
 	modified_by integer null,
