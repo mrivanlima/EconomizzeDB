17948d7ed0a9229bd3370a9319434c47577e3fa3
Ivan Lima <mrivanlima@gmail.com>
2024-08-09 18:35:23 -0400

    email

--- app/Tables/user.sql
+++ app/Tables/user.sql
@@ -4,7 +4,6 @@ create table app.user
 	user_first_name varchar(100),
 	user_middle_name varchar(100) null,
 	user_last_name varchar(100) null,
-	-- user_email varchar(250) not null,
 	cpf char(11) null,
 	rg  varchar(100) null,
 	date_of_birth date null,  
@@ -14,7 +13,6 @@ create table app.user
 	modified_by integer null,
 	modified_on timestamp with time zone default current_timestamp,
 	constraint pk_user primary key (user_id),
-	-- constraint uk_user_email unique (user_email),
 	constraint fk_user_login_user foreign key (user_id) references app.user_login(user_id),
 	constraint fk_user_created_by foreign key (created_by) references app.user_login(user_id),
 	constraint fk_user_modified_by foreign key (modified_by) references app.user_login(user_id)
